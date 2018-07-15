bcbc24314dd37a7f6f3e463c5d44420df326095a
AlexPolubiakin <alpolubnev@gmail.com>
2018-07-15 12:38:49 +0300

    final

--- client.php
+++ client.php
@@ -1,2 +1,39 @@
-<?php 
-
+<meta charset="utf-8">
+<?php
+	try {
+		$client = new SoapClient("https://soap-server-polubiakin.c9users.io/goods.wsdl");
+		
+		echo 'Поиск по Title = "Lenovo Vibe Shot"<br>';
+		if($client->getGoodsByTitle("Lenovo Vibe Shot")){
+		    echo "Товар есть в наличии<br><hr>";
+		} else {
+		    echo "Увы такого товара нет в наличии<br><hr>";
+		}
+	
+	    
+	    echo 'Поиск по Manufacturer = "Lenovo123"<br>';
+		if($client->getGoodsByManufacturer("Lenovo123")){
+		    echo "Такой производитель есть в наличии<br><hr>";
+		} else {
+		    echo "Увы такого производителя нет в наличии<br><hr>";
+		}
+	    
+	    echo 'Поиск по Price = 10e4 <br>';
+		if($client->getGoodsByPrice(123e4)){
+		    echo "Товар с такой ценой есть в наличии<br><hr>";
+		} else {
+		    echo "Товара с такой ценой увы нет в наличии<br><hr>";
+		}
+		
+		echo 'Поиск по Color = "black"<br>';
+		if($client->getGoodsByColor("black")){
+		    echo "Товар есть в наличии<br><hr>";
+		} else {
+		    echo "Увы такого товара нет в наличии<br><hr>";
+		}
+	    
+	} catch (SoapFault $exception) {
+		echo $exception->getMessage();	
+    }
+    
+?>
--- server.php
+++ server.php
@@ -1,35 +1,60 @@
 <?php 
 
 /**
- *  SOAP SERVER CLASS
- *  https://gist.github.com/umidjons/f3de2533c51495a9c557
+ *  SOAP SERVER
  */
 
+ class Goods {
 
- /**
- * 
- * Создать базу данных этим товаром
- * // [ "title" => "Lenovo Vibe Shot", "manufacturer" => "Lenovo", "price" => 9e3, "color" => "black"],
- * [ "title" => "Apple iPhone 7 128GB", "manufacturer" => "Apple", "price" => 9e4, "color" => "white" ],
- * [ "title" => "Samsung Galaxy S8", "manufacturer" => "Samsung", "price" => 5e4, "color" => "grey" ],
- * [ "title" => "Lenovo P2", "manufacturer" => "Lenovo", "price" => 2e4, "color" => "grey" ],
- * [ "title" => "Xiaomi Mi6 128GB", "manufacturer" => "Xiaomi", "price" => 7e3, "color" => "white" ],
- * [ "title" => "Xiaomi Mi Note 3 64Gb", "manufacturer" => "Xiaomi", "price" => 2e4, "color" => "black" ],
- * ]
- * Необходимо реализовать методы в классе:
- * Которые вытаскивают из бд по значениям
- * title
- * manufacturer
- * price
- * color
- * 
- * написать wsdl документ
- * 
- * Понять как связывается сервер и клиент , судя по всему на основе wdsl документа 
- * 
- * создать на другом хосте или на другом сервере простую страницу где выводяться по 1 значению title manufacturer price color 
- * 
- */
+    public $goods = [
+        [ "title" => "Lenovo Vibe Shot", "manufacturer" => "Lenovo", "price" => 9e3, "color" => "black"],
+        [ "title" => "Apple iPhone 7 128GB", "manufacturer" => "Apple", "price" => 9e4, "color" => "white" ],
+        [ "title" => "Samsung Galaxy S8", "manufacturer" => "Samsung", "price" => 5e4, "color" => "grey" ],
+        [ "title" => "Lenovo P2", "manufacturer" => "Lenovo", "price" => 2e4, "color" => "grey" ],
+        [ "title" => "Xiaomi Mi6 128GB", "manufacturer" => "Xiaomi", "price" => 7e3, "color" => "white" ],
+        [ "title" => "Xiaomi Mi Note 3 64Gb", "manufacturer" => "Xiaomi", "price" => 2e4, "color" => "black" ],
+ ];
+
+
+    function getGoodsByTitle($name){
+        foreach($this->goods as $key => $value){
+            if($this->goods[$key]['title'] == $name){
+                $result[] = $value;
+            }
+        }    
+        return $result;    
+    }
+
+    function getGoodsByManufacturer($manufacturer){
+        foreach($this->goods as $key => $value){
+            if($this->goods[$key]['manufacturer'] == $manufacturer){
+                $result[] = $value;
+            }
+        }    
+        return $result;
+    }
+
+    function getGoodsByPrice($price){
+        foreach($this->goods as $key => $value){
+            if($this->goods[$key]['price'] == $price){
+                $result[] = $value;
+            }
+        }    
+        return $result;
+        
+    }
+
+    function getGoodsByColor($color){
+        foreach($this->goods as $key => $value){
+            if($this->goods[$key]['color'] == $color){
+                $result[] = $value;
+            }
+        }    
+        return $result;
+        
+    }
+ }
+
 
 
 // Отключение кеширования wsdl-документа
@@ -37,24 +62,29 @@ ini_set("soap.wsdl_cache_enabled", "0");
 // Создание SOAP-сервера
 $server = new SoapServer("http://localhost/soap/goods.wsdl");
 // Регистрация класса
-$server->setClass("GoodsInfo");
+$server->setClass("Goods");
 // Запуск сервера
 $server->handle();
 
 
+//  $test = new Goods;
+// //  print_r($test->getGoodsByTitle('Lenovo P2'));
+//  if($test->getGoodsByTitle('Lenovo P2')){
+//      echo 'есть в наличии';
+//  } else {
+//      echo 'нет в наличии';
+//  }
 
- class GoodsInfo {
+//  echo "<br>";
 
-    function getItemsByName(){
+//  print_r($test->getGoodsByManufacturer('Xiaomi'));
 
-    }
-    function getItemsByManufacturer(){
-        
-    }
-    function getItemsByPrice(){
-        
-    }
-    function getItemsByColor(){
-        
-    }
- }
+//  echo "<br>";
+
+//  print_r($test->getGoodsByPrice(5e4));
+
+//  echo "<br>";
+
+//  print_r($test->getGoodsByColor('black'));
+
+phpinfo();
